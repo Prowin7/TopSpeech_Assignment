36f85d6fb4b49ac903fdb7e2808991dbcabc3e70
Vishwanth <vishwanthmunikuntla@gmail.com>
2026-02-12 00:37:30 +0530

    Initial Commit

---
+++ streaks/streaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  streaksApp.swift
+//  streaks
+//
+//  Created by vishwanth munikuntla on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct streaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ streaksTests/streaksTests.swift
@@ -0,0 +1,17 @@
+//
+//  streaksTests.swift
+//  streaksTests
+//
+//  Created by vishwanth munikuntla on 12/02/26.
+//
+
+import Testing
+@testable import streaks
+
+struct streaksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
